8c10962993c6f6be573036115165aa1832354522
smithwebtek <brad@smithwebtek.com>
2018-11-05 06:18:09 -0500

    Wrap up

--- src/adapters/NotesAdapter.js
+++ src/adapters/NotesAdapter.js
@@ -1,12 +1,12 @@
 class NotesAdapter {
 	constructor() {
-		this.baseUrl = 'http://localhost:3000/api/v1'
+		this.baseUrl = 'http://localhost:3000/api/v1/notes'
 		// this.baseUrl = 'https://swt-notes-api.herokuapp.com/api/v1/notes'
 		this.baseUrl2 = 'https://music-db-api.herokuapp.com/api/songs'
 	}
 
 	getNotes() {
-		return fetch(this.baseUrl + '/notes').then(res => {
+		return fetch(this.baseUrl).then(res => {
 			return res.json()
 		})
 	}
@@ -16,43 +16,38 @@ class NotesAdapter {
 		const note = {
 			body: value
 		}
-		return fetch(this.baseUrl + '/notes', {
+		return fetch(this.baseUrl, {
 			method: 'post',
-			cors: 'cors',
+			// cors: 'cors',
 			headers: { "Content-Type": "application/json; charset=utf-8" },
 			body: JSON.stringify({ note }) // equivalent in ES6 to: {note: note } because the key and value are same
 		}).then(res => res.json())
 	}
 
-	editNoteForm(id, body) {
-		event.preventDefault()
-		console.log('id: ', id);
-		console.log('body: ', body);
+	updateNote(note) {
+		let url = this.baseUrl + `/${note.id}`
 
-		let editNoteForm = document.getElementById('edit-note-form')
-		editNoteForm.style = { visibility: true }
-
-		let editNoteBody = document.getElementById('edit-note-body').value
-
-		this.updateNote(id, editNoteBody)
-	}
-
-	updateNote(id, body) {
-		event.preventDefault()
-		const note = {
-			id: id,
-			body: body
-		}
-
-		return fetch(this.baseUrl + `/notes/${id}`, {
-			method: 'patch',
+		return fetch(url, {
+			method: 'PATCH',
 			headers: {
 				"Content-Type": "application/json; charset=utf-8"
 			},
-			body: JSON.stringify({ note })
-		}).then(res => res.json())
+			body: JSON.stringify(note),
+		}).then(res => res.json());
 	}
 
+	deleteNote(id) {
+		console.log('delete id: ', id);
+		let url = this.baseUrl + `/${id}`
+
+		return fetch(url, {
+			method: 'DELETE',
+			headers: {
+				"Content-Type": "application/json; charset=utf-8"
+			},
+			body: JSON.stringify(id),
+		}).then(res => res.json());
+	}
 
 	getSongs() {
 		return fetch(this.baseUrl2).then(res => res.json())
--- src/components/notes.js
+++ src/components/notes.js
@@ -16,15 +16,37 @@ class Notes {
 		this.newNoteBody = document.getElementById('new-note-body')
 		this.noteForm.addEventListener('submit', this.createNote.bind(this))
 		this.notesContainer.addEventListener('dblclick', this.handleNoteClick.bind(this))
+
 		// this.notesContainer.addEventListener('blur', this.updateNote.bind(this), true) // worked
-		this.body.addEventListener('blur', this.updateNote.bind(this), true)
+		this.updateButton = document.getElementById('update-button')
+		this.notesContainer.addEventListener('blur', this.updateNote.bind(this), true)
 	}
 
 	handleNoteClick(e) {
+		this.toggleNote(e)
+	}
+
+	toggleNote(e) {
 		const li = e.target
 		li.contentEditable = true
 		li.classList.add('editable')
+		this.updateButton.classList.add('showing');
+		this.updateButton.classList.remove('not-showing');
 		li.focus()
+	}
+
+	updateNote(e) {
+		e.preventDefault()
+		const li = e.target
+		li.contentEditable = false
+		li.classList.remove('editable')
+		this.updateButton.classList.remove('showing');
+		this.updateButton.classList.add('not-showing');
+
+		let body = e.target.textContent
+		let id = e.target.dataset.id
+		let note = { id, body }
+		this.adapter.updateNote(note)
 	}
 
 	createNote(e) {
@@ -38,42 +60,33 @@ class Notes {
 			})
 	}
 
-	updateNote(e) {
-		e.preventDefault()
-		console.log('edit note ...', event.target);
-		let body = e.target.textContent
-		let id = e.target.dataset.id
-		this.adapter.updateNote(id, body)
-	}
-
 	fetchAndLoadNotes() {
 		this.adapter.getNotes()
 			.then(notes => {
-				notes.forEach(note => this.notes.push(new Note(note)))
+				// notes.forEach(note => this.notes.push(new Note(note)))
+				notes.sort((a, b) => a.id - b.id).forEach(note => this.notes.push(new Note(note)))
 			})
 			.then(() => {
 				this.render()
 			})
 	}
 
-	// 1
+	// 1 ----------------------------------------------------------------------------
 	// render() {
 	// 	let notesContainer = document.getElementById('notes-container')
 	// 	this.notes.map((note, index) => {
 	// 		// notesContainer.innerHTML += (`<li note-id=${index}>${note.body}</li>`)
-	// 		notesContainer.innerHTML += (`<li note-id=${index}>${note.body}</li>`)
 	// 	})
 	// }
 
-	// 2
+	// 2 ----------------------------------------------------------------------------
 	// render() {
 	// 	const notesString = this.notes.map(note => `<li>${note.body}</li>`)
-	// 	console.log(notesString);
 	// 	console.log(notesString.join(''));
 	// 	this.notesContainer.innerHTML = notesString.join('')
 	// }
 
-	// 3
+	// 3 ----------------------------------------------------------------------------
 	render() {
 		// this.newNoteBody.value = ''
 		const notesList = this.notes.map(n => n.renderLi()).join('')
